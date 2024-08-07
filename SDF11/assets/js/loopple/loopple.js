677853d0afaff0ecbe01e4635e9aa5c1d648da64
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-07 23:28:37 +0200

    Fix alert dismissal for multiple buttons and specific alerts
    Updated code to handle multiple '.close' buttons with 'querySelectorAll'. Corrected alert targeting to ensure the specific alert associated with the clicked button is removed. Improved code efficiency and clarity by using 'forEach' for event binding.

--- SDF11/assets/js/loopple/loopple.js
+++ SDF11/assets/js/loopple/loopple.js
@@ -1,8 +1,13 @@
 
 // Alert dismissible function
-document.querySelector('.loopple-alert.loopple-alert-dismissible .close').onclick = function() {
- document.querySelector('.loopple-alert').classList.add('loopple-opacity-0');
- setTimeout(function(){
-   document.querySelector('.loopple-alert').remove();
- }, 1000);
-}
+document.querySelectorAll('.loopple-alert.loopple-alert-dismissible .close').forEach(function(button) {
+  button.onclick = function() {
+    var alert = this.closest('.loopple-alert'); // Find the closest parent with the alert class
+    alert.classList.add('loopple-opacity-0'); // Apply the opacity class
+    
+    setTimeout(function() {
+      alert.remove(); // Remove the specific alert
+    }, 1000); // Ensure this timeout matches the CSS transition duration
+  };
+});
+
